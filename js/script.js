e1b6310f322bce72143c27e57f9301ac283c14db
SurelyEnough <ddale1128@gmail.com>
2022-05-12 15:01:09 -0700

    Add quiz functionality and interface

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 function hideLayer(id) {
-    var e = document.getElementById(id)
-    var buttone = document.getElementById(id + "-button")
-    e.style.display = 'none'
-    buttone.classList.remove("selected")
-    buttone.classList.add("unselected")
+  var e = document.getElementById(id);
+  var buttone = document.getElementById(id + "-button");
+  e.style.display = "none";
+  buttone.classList.remove("selected");
+  buttone.classList.add("unselected");
 }
 
 function showLayer(id) {
-    var e = document.getElementById(id)
-    var buttone = document.getElementById(id + "-button")
-    if (e.style.display == 'none')
-        e.style.display = 'block'
-    buttone.classList.remove("unselected")
-    buttone.classList.add("selected")
+  var e = document.getElementById(id);
+  var buttone = document.getElementById(id + "-button");
+  if (e.style.display == "none") e.style.display = "block";
+  buttone.classList.remove("unselected");
+  buttone.classList.add("selected");
 }
